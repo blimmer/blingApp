4a3ce03c4fa95f9d7d38a8fb3dbfe721cfbe27c4
Eric Baer <eric.baer@readytalk.com>
2013-03-24 00:02:15 -0600

    Added Bootstrap, Cleaned up whitespace, fixed star centering correctly, removed dead code
    	new file:   www/css/bootstrap.min.css
    	modified:   www/css/index.css
    	modified:   www/index.html
    	modified:   www/js/index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,60 +1,38 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
+  // Application Constructor
+  initialize: function() {
+      this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicity call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+      app.showButtons();
+  },
+  camera: {
+    getPhoto: function(source) {
+      navigator.camera.getPicture(app.camera.success, app.camera.fail,
+                                  {
+                                    quality: 50,
+                                    destinationType: Camera.DestinationType.FILE_URI,
+                                    sourceType: source
+                                  }
+                                );
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    success: function(imageURI) {
+      var canvas = document.getElementsByTagName('canvas')[0];
+      canvas.style.background = "url(" + imageURI + ") no-repeat";
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.showButtons();
-    },
-    // Update DOM on a Received Event
-    showButtons: function() {
-        var buttonBar = document.getElementById('btn-group');
-        buttonBar.setAttribute('style', 'display: block');
-        
-        console.log('Received Event: ' + id);
-    },
-    camera: {
-        getPhoto: function(source) {
-            navigator.camera.getPicture(app.camera.success, app.camera.fail,
-                                {quality: 50,
-                                destinationType: Camera.DestinationType.FILE_URI,
-                                sourceType: source});
-        },
-        success: function(imageURI) {
-            var canvas = document.getElementById('canvas-container');
-            canvas.style.background = "url(" + imageURI + ") no-repeat";
-        },
-        fail: function(errorMessage) {
-            alert('capture failed because ' + errorMessage);
-        }
+    fail: function(errorMessage) {
+      alert('capture failed because ' + errorMessage);
     }
+  }
 };
